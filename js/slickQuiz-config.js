9f8d3b9fde7f73b3678cd210dac5303efdaf5926
Lawrence Adams <lawrencetadams@googlemail.com>
2015-04-18 16:11:28 +0100

    Added another question re:ALT and alcohol/hepatiti, will make public now.

--- js/slickQuiz-config.js
+++ js/slickQuiz-config.js
@@ -13,169 +13,204 @@ var quizJSON = {
 		"level5": "What is an LFT?"
 	},
 	"questions": [{ // Question - Multiple Choice, Single True Answer
-			"q": "Alanine Transaminase (<em>ALT</em>) and Aspartate aminotransferase (<em>AST</em>) are:",
-			"a": [{
-				"option": "Hepatic enzymes",
+		"q": "Alanine Transaminase (<em>ALT</em>) and Aspartate aminotransferase (<em>AST</em>) are:",
+		"a": [{
+			"option": "Hepatic enzymes",
+			"correct": true
+		}, {
+			"option": "Indicate a degree of Hepatic inflammation",
+			"correct": true
+		}, {
+			"option": "Low in Hepatitis",
+			"correct": false
+		}, {
+			"option": "<strong>ALT</strong> is also present in muscle cells",
+			"correct": false
+		}],
+		"select-any": true,
+		"correct": "<p><span>Correct!</span>",
+		"incorrect": "<p><span>Incorrect.</span><strong>ALT</strong> and <strong>AST</strong> are both hepatic enzymes, although AST is less specific <em>(as it is also present in muscle cells)</em><br>Both ALT and AST indicate a degree of inflamation and are high in hepatitis!<br></p>"
+	}, {
+		"q": "An ALT greater than 1000 U/L is likely to be:",
+		"a": [
+		{
+			"option": "Hepatitis",
+			"correct": true
+		}, {
+			"option": "Alcoholic Liver Disease",
+			"correct": false
+		}],
+		"correct": "<p><span>Correct!</span>A very high ALT is likely to be a hepatitis of some form (<em>autoimmune, drug incduced, viral</em>).</p>",
+		"incorrect": "<p><span>Incorrect.</span>A very high ALT is likely to be a hepatitis of some form (<em>autoimmune, drug incduced, viral</em>).</p>"
+	}, { // True/false
+		"q": "Alkaline phosphatase(<em>ALP</em>) is a cholestatic enzyme?",
+		"a": [{
+				"option": "Yes",
 				"correct": true
 			}, {
-				"option": "Indicate a degree of Hepatic inflammation",
+				"option": "No",
+				"correct": false
+			} // no comma here
+		],
+		"correct": "<p><span>Correct!</span>Well done!</p>",
+		"incorrect": "<p><span>Incorrect.</span>ALP is found mostly in the <strong>bile ducts</strong> (also bone, and as such is raised during adolescence and pregnancy) of the liver.<br>When elevated indicated either <strong>obstuctive or cholestatic</strong> liver disease<br></p>"
+	}, {
+		"q": "What happens to the levels of ALP in patients with hepatic metastases or primary billiary cirrhosis",
+		"a": [{
+			"option": "ALP is <strong>increased</strong>",
+			"correct": true
+		}, {
+			"option": "ALP is <strong>decreased</strong>",
+			"correct": false
+		}],
+		"correct": "<p><span>Correct!</span>Well done!</p>",
+		"incorrect": "<p><span>Incorrect.</span><strong>ALP</strong> will be increased in these two cases."
+	}, {
+		"q": "Select the true statement about &gamma;GT:",
+		"a": [{
+			"option": "GGT does not rise with cholestasis.",
+			"correct": false
+		}, {
+			"option": "GGT can be used as a guide of alcohol intake if ALT is normal.",
+			"correct": true
+		}],
+		"correct": "<p><span>Correct!</span>Well done</p>",
+		"incorrect": "<p><span>Incorrect.</span><strong>ALT</strong> can be used as a guide of alcohol intake.<br>It also rises with cholestatis, but does not indicate liver disease is other tests are normal."
+	}, {
+		"q": "In acute liver disase, what would you expect?",
+		"a": [{
+				"option": "Normal levels of <strong>Albumin</strong>",
 				"correct": true
 			}, {
-				"option": "Low in Hepatitis",
+				"option": "Decreased levels of <strong>Albumin</strong>",
 				"correct": false
 			}, {
-				"option": "<strong>ALT</strong> is also present in muscle cells",
+				"option": "Normal levels of <strong>Prothrombin</strong>",
 				"correct": false
-			}],
-			"select-any": true,
-			"correct": "<p><span>Correct!</span>",
-			"incorrect": "<p><span>Incorrect.</span>ALT and AST are both hepatic enzymes, although AST is less specific <em>(as it is also present in muscle cells)</em><br>Both ALT and AST indicate a degree of inflamation and are high in hepatitis!<br></p>"
-		},
+			}, {
+				"option": "Decreasd levels of <strong>Prothrombin</strong>",
+				"correct": true
+			}
 
-		{ // True/false
-			"q": "Alkaline phosphatase(<em>ALP</em>) is a cholestatic enzyme?",
-			"a": [{
-					"option": "Yes",
-					"correct": true
-				}, {
-					"option": "No",
-					"correct": false
-				} // no comma here
-			],
-			"correct": "<p><span>Correct!</span>Well done!</p>",
-			"incorrect": "<p><span>Incorrect.</span>ALP is found mostly in the <strong>bile ducts</strong> (also bone, and as such is raised during adolescence and pregnancy) of the liver.<br>When elevated indicated either <strong>obstuctive or cholestatic</strong> liver disease<br></p>"
+		],
+		"select-any": true,
+		"correct": "<p><span>Correct!</span>Well done</p>",
+		"incorrect": "<p><span>Incorrect.</span><strong>Albumin</strong> has a relatively long half life compared to <strong>Prothrombin</strong>, therefore in <strong><em>acute</em></strong> liver disease you would expect a normal albumin with a low prothrombin (which has a longer half life).</p>"
+	}, { // question template on specific questions
+		"q": "Dark urine rules out post-hepatic jaundice:",
+		"a": [{
+			"option": "True",
+			"correct": false
 		}, {
-			"q": "What happens to the levels of ALP in patients with hepatic metastases or primary billiary cirrhosis",
-			"a": [{
-				"option": "ALP is <strong>increased</strong>",
-				"correct": true
-			}, {
-				"option": "ALP is <strong>decreased</strong>",
-				"correct": false
-			}],
-			"correct": "<p><span>Correct!</span>Well done!</p>",
-			"incorrect": "<p><span>Incorrect.</span><strong>ALP</strong> will be increased in these two cases."
+			"option": "False",
+			"correct": true
+		}],
+		"correct": "<p><span>Correct!</span>Well done</p>",
+		"incorrect": "<p><span>Incorrect.</span><strong>Dark</strong> urine is usually indicative of intra, or post-hepatic jaundice (bilirubin is not eliminated in the stool), and rules out pre-hepatic jaundice.</p>"
+	}, {
+		"q": "Malnutrition can cause this maker to decrease:",
+		"a": [{
+			"option": "Alanine Transaminase (<strong>ALT</strong>)",
+			"correct": false
 		}, {
-			"q": "Select the true statement about &gamma;GT:",
-			"a": [{
-				"option": "GGT does not rise with cholestasis.",
-				"correct": false
-			}, {
-				"option": "GGT can be used as a guide of alcohol intake if ALT is normal.",
-				"correct": true
-			}],
-			"correct": "<p><span>Correct!</span>Well done</p>",
-			"incorrect": "<p><span>Incorrect.</span><strong>ALT</strong> can be used as a guide of alcohol intake.<br>It also rises with cholestatis, but does not indicate liver disease is other tests are normal."
+			"option": "Aspartate aminotransferase (<strong>AST</strong>)",
+			"correct": false
 		}, {
-			"q": "In acute liver disase, what would you expect?",
-			"a": [{
-					"option": "Normal levels of <strong>Albumin</strong>",
-					"correct": true
-				}, {
-					"option": "Decreased levels of <strong>Albumin</strong>",
-					"correct": false
-				}, {
-					"option": "Normal levels of <strong>Prothrombin</strong>",
-					"correct": false
-				}, {
-					"option": "Decreasd levels of <strong>Prothrombin</strong>",
-					"correct": true
-				}
-
-			],
-			"select-any": true,
-			"correct": "<p><span>Correct!</span>Well done</p>",
-			"incorrect": "<p><span>Incorrect.</span><strong>Albumin</strong> has a relatively long half life compared to <strong>Prothrombin</strong>, therefore in <em>acute</em> liver disease you would expect a normal albumin with a low prothrombin (which has a longer half life)</p>"
-		}, { // question template on specific questions
-			"q": "Dark urine rules out post-hepatic jaundice:",
-			"a": [{
-				"option": "True",
-				"correct": false
-			}, {
-				"option": "False",
-				"correct": true
-			}],
-			"correct": "<p><span>Correct!</span>Well done</p>",
-			"incorrect": "<p><span>Incorrect.</span><strong>Dark</strong> urine is usually indicative of intra, or post-hepatic jaundice (bilirubin is not eliminated in the stool), and rules out pre-hepatic jaundice.</p>"
+			"option": "Alkaline phosphatase (<strong>ALP</strong>)",
+			"correct": false
 		}, {
-			"q": "Elevated levels of this enzymes indicates obstruction or cholestatic liver disease:",
-			"a": [{
-				"option": "Alanine Transaminase (<strong>ALT</strong>)",
-				"correct": false
-			}, {
-				"option": "Aspartate aminotransferase (<strong>AST</strong>)",
-				"correct": false
-			}, {
-				"option": "Alkaline phosphatase (<strong>ALP</strong>)",
-				"correct": true
-			}, {
-				"option": "Gamma-glutamyltransferase (<strong>GGT</strong>)",
-				"correct": false
-			}, {
-				"option": "Albumin",
-				"correct": false
-			}, {
-				"option": "Bilirubin",
-				"correct": false
-			}, {
-				"option": "Prothrombin time (<strong>PT</strong>)",
-				"corect": false
-			}],
-			"correct": "<p><span>Correct!</span>Well done!</p>",
-			"incorrect": "<p><span>Incorrect.</span><strong>ALP</strong> is a cholestatic enzyme that would be eleveated.</p>"
+			"option": "Gamma-glutamyltransferase (<strong>GGT</strong>)",
+			"correct": false
 		}, {
-			"q": "Which enzymes leak into the blood stream when liver cells are damaged?",
-			"a": [{
-				"option": "Alanine Transaminase (<strong>ALT</strong>)",
-				"correct": true
-			}, {
-				"option": "Aspartate aminotransferase (<strong>AST</strong>)",
-				"correct": true
-			}, {
-				"option": "Alkaline phosphatase (<strong>ALP</strong>)",
-				"correct": false
-			}, {
-				"option": "Gamma-glutamyltransferase (<strong>GGT</strong>)",
-				"correct": false
-			}, {
-				"option": "Albumin",
-				"correct": false
-			}, {
-				"option": "Bilirubin",
-				"correct": false
-			}, {
-				"option": "Prothrombin time (<strong>PT</strong>)",
-				"corect": false
-			}],
-			"select-any": true,
-			"correct": "<p><span>Correct!</span>Well done!</p>",
-			"incorrect": "<p><span>Incorrect.</span><strong>AST</strong> and <strong>ALT</strong> would be elevated.</p>"
+			"option": "Albumin",
+			"correct": true
 		}, {
-			"q": "A test of clotting function:",
-			"a": [{
-				"option": "Alanine Transaminase (<strong>ALT</strong>)",
-				"correct": false
-			}, {
-				"option": "Aspartate aminotransferase (<strong>AST</strong>)",
-				"correct": false
-			}, {
-				"option": "Alkaline phosphatase (<strong>ALP</strong>)",
-				"correct": false
-			}, {
-				"option": "Gamma-glutamyltransferase (<strong>GGT</strong>)",
-				"correct": false
-			}, {
-				"option": "Albumin",
-				"correct": false
-			}, {
-				"option": "Bilirubin",
-				"correct": false
-			}, {
-				"option": "Prothrombin time (<strong>PT</strong>)",
-				"corect": true
-			}],
-			"correct": "<p><span>Correct!</span>Well done!</p>",
-			"incorrect": "<p><span>Incorrect.</span>Prothrombin time (or <strong>PT</strong>) is a marker of clotting function, and is a <em>true</em> liver function test."
-		}
-	]
+			"option": "Bilirubin",
+			"correct": false
+		}, {
+			"option": "Prothrombin time (<strong>PT</strong>)",
+			"corect": false
+		}],
+		"correct": "<p><span>Correct!</span>Well done!</p>",
+		"incorrect": "<p><span>Incorrect.</span>Albumin is made up of amino acids from ingested protein in the liver.<br>Can also be decreased by: Diarrhoea, Liver disease, Iron deficiency, Infection.</p>"
+	}, {
+		"q": "Elevated levels of this enzymes indicates obstruction or cholestatic liver disease:",
+		"a": [{
+			"option": "Alanine Transaminase (<strong>ALT</strong>)",
+			"correct": false
+		}, {
+			"option": "Aspartate aminotransferase (<strong>AST</strong>)",
+			"correct": false
+		}, {
+			"option": "Alkaline phosphatase (<strong>ALP</strong>)",
+			"correct": true
+		}, {
+			"option": "Gamma-glutamyltransferase (<strong>GGT</strong>)",
+			"correct": false
+		}, {
+			"option": "Albumin",
+			"correct": false
+		}, {
+			"option": "Bilirubin",
+			"correct": false
+		}, {
+			"option": "Prothrombin time (<strong>PT</strong>)",
+			"corect": false
+		}],
+		"correct": "<p><span>Correct!</span>Well done!</p>",
+		"incorrect": "<p><span>Incorrect.</span><strong>ALP</strong> is a cholestatic enzyme that would be eleveated.</p>"
+	}, {
+		"q": "Which enzymes leak into the blood stream when liver cells are damaged?",
+		"a": [{
+			"option": "Alanine Transaminase (<strong>ALT</strong>)",
+			"correct": true
+		}, {
+			"option": "Aspartate aminotransferase (<strong>AST</strong>)",
+			"correct": true
+		}, {
+			"option": "Alkaline phosphatase (<strong>ALP</strong>)",
+			"correct": false
+		}, {
+			"option": "Gamma-glutamyltransferase (<strong>GGT</strong>)",
+			"correct": false
+		}, {
+			"option": "Albumin",
+			"correct": false
+		}, {
+			"option": "Bilirubin",
+			"correct": false
+		}, {
+			"option": "Prothrombin time (<strong>PT</strong>)",
+			"corect": false
+		}],
+		"select-any": true,
+		"correct": "<p><span>Correct!</span>Well done!</p>",
+		"incorrect": "<p><span>Incorrect.</span><strong>AST</strong> and <strong>ALT</strong> would be elevated.</p>"
+	}, {
+		"q": "A test of clotting function:",
+		"a": [{
+			"option": "Alanine Transaminase (<strong>ALT</strong>)",
+			"correct": false
+		}, {
+			"option": "Aspartate aminotransferase (<strong>AST</strong>)",
+			"correct": false
+		}, {
+			"option": "Alkaline phosphatase (<strong>ALP</strong>)",
+			"correct": false
+		}, {
+			"option": "Gamma-glutamyltransferase (<strong>GGT</strong>)",
+			"correct": false
+		}, {
+			"option": "Albumin",
+			"correct": false
+		}, {
+			"option": "Bilirubin",
+			"correct": false
+		}, {
+			"option": "Prothrombin time (<strong>PT</strong>)",
+			"corect": true
+		}],
+		"correct": "<p><span>Correct!</span>Well done!</p>",
+		"incorrect": "<p><span>Incorrect.</span>Prothrombin time (or <strong>PT</strong>) is a indicator of clotting function, and is a <em>true</em> liver function test."
+	}]
 };
